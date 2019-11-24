cdfea9186b70ae5c2599eb9442c5d18f1869c83e
wanghuiwen312@sina.com <wanghuiwen312@sina.com>
2019-11-24 11:41:26 +0800

    app release

--- mall-web/src/test/java/CodeGenerator.java
+++ mall-web/src/test/java/CodeGenerator.java
@@ -18,7 +18,7 @@ import static com.api.config.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/blog?useInformationSchema=true&nullCatalogMeansCurrent=true";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/blog?useInformationSchema=true&nullCatalogMeansCurrent=true&&serverTimezone=Asia/Shanghai";
     private static final String JDBC_USERNAME = "root";
     private static final String JDBC_PASSWORD = "123456";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
@@ -40,7 +40,7 @@ public class CodeGenerator {
     private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
 
     public static void main(String[] args) {
-        genCode(true,true,"app发布历史","app_history");
+        genCode(true,false,"a","app");
 //        genController("user_info",null);
         //genCodeByCustomModelName("输入表名","输入自定义Model名称");
     }
---
+++ release-app/src/main/java/com/api/dao/AppMapper.java
@@ -0,0 +1,7 @@
+package com.api.dao;
+
+import com.api.core.Mapper;
+import com.api.model.App;
+
+public interface AppMapper extends Mapper<App> {
+}
